fb57621219247aed365a81ff326f5b3bcdb05e78
Juan Tellez <juantellez916@gmail.com>
2025-10-28 22:09:59 -0600

    add nativewind configuration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "@/navigation/StackNavigator";
+import "./global.css"
 
 export default function App() {
   return <NavigationContainer>
--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,6 +1,6 @@
-import { Text, View, Button } from "react-native";
+import { Text, View } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
-import { RootStackParamList } from "@/navigation/StackNavigator";
+import { RootStackParamList } from "@/../types";
 
 type LoginScreenNavigationProp = StackScreenProps<RootStackParamList, "Login">;
 interface LoginScreenProps extends LoginScreenNavigationProp { }
@@ -8,8 +8,7 @@ interface LoginScreenProps extends LoginScreenNavigationProp { }
 export default function Login({ navigation }: LoginScreenProps) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Login Screen</Text>
-      <Button title="Login" onPress={() => navigation.replace("Home")} />
+      <Text className="color-red-700">Login Screen</Text>
     </View>
   );
 }
