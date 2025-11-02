2382df61a5aa7a84b25bddf2d9ca5597f957bf0a
jntellez <juantellez916@gmail.com>
2025-11-02 15:13:28 -0600

    feat: implement TabNavigator with Home, Documents, and Settings screens

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import { NavigationContainer } from "@react-navigation/native";
-import StackNavigator from "@/navigation/StackNavigator";
-import "./global.css"
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import TabNavigator from '@/navigation/TabNavigator';
+import "./global.css";
 
 export default function App() {
-  return <NavigationContainer>
-    <StackNavigator />
-  </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
+      <TabNavigator />
+    </NavigationContainer>
+  );
 }
+
